59d3aee38a15360e0c63f87a02bf9fba89363aff
flippycereal <brodiebabcock1@gmail.com>
2023-11-03 22:09:06 -0400

    Added the ModID and allows the game to run

--- src/main/java/net/flippycereal/infernitemod/Infernite.java
+++ src/main/java/net/flippycereal/infernitemod/Infernite.java
@@ -3,6 +3,7 @@ package net.flippycereal.infernitemod;
 import net.fabricmc.api.ModInitializer;
 
 public class Infernite implements ModInitializer {
+    public static final String MOD_ID = "infernitemod";
     /**
      * Runs the mod initializer.
      */
