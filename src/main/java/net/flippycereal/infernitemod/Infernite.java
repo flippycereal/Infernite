4644da6f2adbb935046d7c5518f1a82b2fe6b437
flippycereal <brodiebabcock1@gmail.com>
2023-11-03 21:53:13 -0400

    Initial commit

---
+++ src/main/java/net/flippycereal/infernitemod/Infernite.java
@@ -0,0 +1,13 @@
+package net.flippycereal.infernitemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Infernite implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
